4a8ba9100de92a0905e5c25a6d219a9ea05ff768
2pore <jgaylor84@gmail.com>
2022-01-15 22:28:35 -0600

    First Commit for project.

---
+++ src/main/java/com/cnm/poreair/depinjection/controllers/MyController.java
@@ -0,0 +1,8 @@
+package com.cnm.poreair.depinjection.controllers;
+
+public class MyController {
+    public String sayHello(){
+        System.out.println("Hello World!!!");
+        return "Hi Folks";
+    }
+}
